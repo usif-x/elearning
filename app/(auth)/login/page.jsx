a24c6601a1397fae1ab7b61598dd567f359c42b1
usifk <me@usif.tech>
2025-11-14 21:48:55 +0200

    feat: Update Telegram bot login identifier in login and register components

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -35,7 +35,7 @@ export default function TelegramLoginPage() {
       script.id = "telegram-login-script";
       script.async = true;
       script.src = "https://telegram.org/js/telegram-widget.js?22";
-      script.setAttribute("data-telegram-login", "DahehetHelwanBot");
+      script.setAttribute("data-telegram-login", "ELearningApplicationBot");
       script.setAttribute("data-size", "large");
       script.setAttribute("data-onauth", "onTelegramAuth(user)");
       script.setAttribute("data-request-access", "write");
