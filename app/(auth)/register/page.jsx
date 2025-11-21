5f66549f4c3b00d6bdf45c4f6ce4c338ffe0e75e
usifk <me@usif.tech>
2025-11-21 04:24:26 +0200

    feat: update Telegram bot references in login, register, and notifications components

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -43,7 +43,7 @@ export default function TelegramRegisterPage() {
       script.id = "telegram-login-script";
       script.async = true;
       script.src = "https://telegram.org/js/telegram-widget.js?22";
-      script.setAttribute("data-telegram-login", "ElearningApplicationBot");
+      script.setAttribute("data-telegram-login", "DahhehetMedicalBot");
       script.setAttribute("data-size", "large");
       script.setAttribute("data-onauth", "onTelegramAuth(user)");
       script.setAttribute("data-request-access", "write");
