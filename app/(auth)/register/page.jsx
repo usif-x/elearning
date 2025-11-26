9d2c318bcd33a969ecc14b11af98d098c644359a
usifk <me@usif.tech>
2025-11-26 18:06:26 +0200

    feat(auth): update Telegram bot name in registration page

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -43,7 +43,7 @@ export default function UnifiedRegisterPage() {
           script.id = "telegram-login-script";
           script.async = true;
           script.src = "https://telegram.org/js/telegram-widget.js?22";
-          script.setAttribute("data-telegram-login", "ElearningApplicationBot"); // Ensure this matches your bot name
+          script.setAttribute("data-telegram-login", "DahhehetMedicalBot"); // Ensure this matches your bot name
           script.setAttribute("data-size", "large");
           script.setAttribute("data-onauth", "onTelegramAuth(user)");
           script.setAttribute("data-request-access", "write");
